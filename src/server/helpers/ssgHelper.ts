6250fe27e8aac979ab793833f618b06994514019
Francis Kintungi <franciskintungi@gmail.com>
2023-03-31 17:37:23 +0300

    Latest

--- src/server/helpers/ssgHelper.ts
+++ src/server/helpers/ssgHelper.ts
@@ -2,12 +2,6 @@ import { createProxySSGHelpers } from "@trpc/react-query/ssg";
 import { prisma } from "~/server/db";
 import { appRouter } from "~/server/api/root";
 import superjson from "superjson";
-<<<<<<< HEAD
-=======
-import Layout from "~/components/Layout";
-import { LoadingSpinner } from "~/components/Loading";
-import PostView from "~/components/PostView";
->>>>>>> 1c3fd360019f67a6310223a2cc5ef63c3bb80ca9
 
 export const generateSSGHelper = () => {
   return createProxySSGHelpers({
